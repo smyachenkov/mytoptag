15185afaec1cb93e18dc9fa700feafb12e8f8b7e
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-03-25 02:08:00 +0300

    Cover InstagramPost, InstagramProfileService and InstagramTagService with tests

---
+++ src/test/java/org/mytoptag/model/InstagramPostTest.java
@@ -0,0 +1,36 @@
+package org.mytoptag.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstagramPostTest {
+
+  @Test
+  public void createsListOfTagsFromText() {
+    String text = "#hello world #travel #photo";
+    assertEquals( 3, new InstagramPost(1L, text).getTags().size(),
+        "Simple ctor can't convert text to tags");
+    assertEquals(3, new InstagramPost(1L, text, 22, "link", "code").getTags().size(),
+        "Full ctor can't convert text to tags");
+  }
+
+  @Test
+  public void acceptsEmptyText() {
+    String text = "";
+    assertEquals( 0, new InstagramPost(1L, text).getTags().size(),
+        "Simple ctor can't process empty text");
+    assertEquals(0, new InstagramPost(1L, text, 22, "link", "code").getTags().size(),
+        "Full ctor can't process empty text");
+  }
+
+  @Test
+  public void acceptsNullText() {
+    String text = null;
+    assertEquals( 0, new InstagramPost(1L, text).getTags().size(),
+        "Simple ctor can't process null text");
+    assertEquals(0, new InstagramPost(1L, text, 22, "link", "code").getTags().size(),
+        "Full ctor can't process null text");
+  }
+
+}
