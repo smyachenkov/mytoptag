c479f0573f54da09a65eb959d511882050f1c894
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-12-16 16:04:04 +0300

    fix checkstyle errors

--- src/main/java/org/mytoptag/repository/InstagramTagRepository.java
+++ src/main/java/org/mytoptag/repository/InstagramTagRepository.java
@@ -70,15 +70,15 @@ public interface InstagramTagRepository extends JpaRepository<InstagramTag, Inte
    */
   @Query(
       nativeQuery = true,
-      value = "select latest.tagid\n" +
-          " from (\n" +
-          " select t.id tagid, max(tc.count_date) date\n" +
-          " from tag t\n" +
-          " left join tagcount tc\n" +
-          "   on tc.tag_id = t.id\n" +
-          " group by t.id) latest\n" +
-          " order by date desc\n" +
-          " limit :size"
+      value = "select latest.tagid\n"
+          + " from (\n"
+          + " select t.id tagid, max(tc.count_date) date\n"
+          + " from tag t\n"
+          + " left join tagcount tc\n"
+          + "   on tc.tag_id = t.id\n"
+          + " group by t.id) latest\n"
+          + " order by date desc\n"
+          + " limit :size"
   )
   List<Integer> findNotUpdated(@Param("size") Integer size);
 
@@ -90,14 +90,14 @@ public interface InstagramTagRepository extends JpaRepository<InstagramTag, Inte
    */
   @Query(
       nativeQuery = true,
-      value = "select count(1)\n" +
-          " from ( \n" +
-          " select t.id tagid, max(tc.count_date) date \n" +
-          " from tag t \n" +
-          " left join tagcount tc\n" +
-          " \ton tc.tag_id = t.id\n" +
-          " group by t.id) latest\n" +
-          " where latest.date is null or latest.date < now() - interval '30 day'"
+      value = "select count(1)\n"
+          + " from ( \n"
+          + " select t.id tagid, max(tc.count_date) date \n"
+          + " from tag t \n"
+          + " left join tagcount tc\n"
+          + " \ton tc.tag_id = t.id\n"
+          + " group by t.id) latest\n"
+          + " where latest.date is null or latest.date < now() - interval '30 day'"
   )
   Integer countNotUpdated();
 
